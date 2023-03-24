cf00be8e068cac8e989898f4670d445d064b5958
fotini <f.batzoni@gmail.com>
2023-03-24 12:55:33 +0200

    Adding changes

--- DemoLibrary/Person.cs
+++ DemoLibrary/Person.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class Class1
+    public class Person
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress {get; set; }
+
+        public string PhoneNumber { get; set; } 
     }
 }
