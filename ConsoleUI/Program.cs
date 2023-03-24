e447de171a54bd0bfe31ce3515ed9dafa55cb71c
fotini <f.batzoni@gmail.com>
2023-03-24 14:43:08 +0200

    Refactoring Program.cs

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,19 +12,19 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            IPerson owner = new Person
-            {
-                FirstName = "Tim",
-                LastName = "Barton",
-                EmailAddress = "tim@testmail.com",
-                PhoneNumber = "555-1212"
-            };
+            IPerson owner = Factory.CreatePerson();
 
-            Chore chore = new Chore
-            {
-                ChoreName = "Take out trash",
-                Owner = owner
-            };
+            owner.FirstName = "Tim";
+            owner.LastName = "Barton";
+            owner.EmailAddress = "tim@testmail.com";
+            owner.PhoneNumber = "555-1212";
+   
+
+            IChore chore = Factory.CreateChore();
+
+            chore.ChoreName = "Take out trash";
+            chore.Owner = owner;
+         
 
             chore.PerformedWork(3);
             chore.PerformedWork(1.5);
