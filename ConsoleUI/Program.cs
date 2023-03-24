b33555e0fa7f41eccd84a3bd40dacd7f34ab8996
fotini <f.batzoni@gmail.com>
2023-03-24 12:39:40 +0200

    Adding Person class

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Person owner = new Person
+            {
+                FirstName ="Tim",
+                LastName ="Barton",
+                EmailAddress="tim@testmail.com",
+                PhoneNumber ="555-1212"
+            }
+
+            
         }
     }
 }
