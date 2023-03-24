088c39bcf28136cb45eaa070ac1b38cc53c2348f
fotini <f.batzoni@gmail.com>
2023-03-24 12:50:41 +0200

    Expanding Chore class

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -13,11 +14,23 @@ namespace ConsoleUI
         {
             Person owner = new Person
             {
-                FirstName ="Tim",
-                LastName ="Barton",
-                EmailAddress="tim@testmail.com",
-                PhoneNumber ="555-1212"
-            }
+                FirstName = "Tim",
+                LastName = "Barton",
+                EmailAddress = "tim@testmail.com",
+                PhoneNumber = "555-1212"
+            };
+
+            Chore chore = new Chore
+            {
+                ChoreName = "Take out trash",
+                Owner = owner
+            };
+
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+
+            Console.ReadLine();
 
             
         }
--- DemoLibrary/Chore.cs
+++ DemoLibrary/Chore.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    internal class Chore
+    public class Chore
     {
+        public string ChoreName { get; set; }   
+
+        public Person Owner { get; set; }
+
+        public double HoursWorked { get; private set; }
+
+        public bool IsComplete { get; private set; }
+
+        public void PerformedWork(double hours)
+        {
+            HoursWorked += hours;
+            Logger log = new Logger();
+            log.Log($"Performwd work on  {ChoreName}");
+        }
+
+        public void CompleteWork()
+        { 
+            IsComplete = true;
+
+            Logger log = new Logger();
+            log.Log($"Completed  {ChoreName}");
+
+            Emailer emailer = new Emailer();
+            emailer.SendEmail(Owner, $"The chore {ChoreName} is completed");
+        }
     }
 }
